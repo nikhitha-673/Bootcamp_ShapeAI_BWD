3cf199933f723bb8b049a3d9170430b972b1f083
nikhitha-673 <85481797+nikhitha-673@users.noreply.github.com>
2021-06-26 20:33:17 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Footer()
+{
+  return(
+    <div>
+       <footer>
+          <p>Copyright by ShapeAi @{new Date().getFullYear()}</p>
+       </footer>
+    </div>
+  );
+}
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Info()
+{
+  return(
+    <div className="note">
+       <h1>Javascrip&React Js</h1>
+           <p>A basic webdevolpment react Js</p>
+    </div>
+  );
+}
+export default Info;
